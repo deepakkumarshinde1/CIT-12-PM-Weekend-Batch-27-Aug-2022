17b2e537959caaa32ef2aae809d0bcd7439026e1
deepakkumarshinde1 <deepakkumarshinde1@gmail.com>
2023-01-14 13:03:31 +0530

    React Basic

---
+++ ReactJS/my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// create functional component
+function App() {
+  return (
+    <>
+      <h1 className="text-green">Hello HTML</h1>
+      <span>Hello</span>
+    </>
+  );
+}
+
+// export
+// module.exports = App
+export default App;
+
+// App Component Main
---
+++ ReactJS/my-app/src/index.js
@@ -0,0 +1,17 @@
+import React from "react"; // es6
+// const React = require('react'); // common js
+
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Node =>
+// const http = require('http') => Common JS
+// import http from  'http
