3438082ac570326385cb62bae1de70b56c53ee9f
deepakkumarshinde1 <deepakkumarshinde1@gmail.com>
2023-03-18 16:28:39 +0530

    all filter opt

--- Advance_Node_JS/zomato-api/Controller/RestaurantController.js
+++ Advance_Node_JS/zomato-api/Controller/RestaurantController.js
@@ -36,17 +36,35 @@ module.exports.filter = async (request, response) => {
   // Cost For Two (optional)
   // Sort (optional)
   // Per Page Data (optional)
-  let data = {
-    mealtype: 1,
-  };
 
+  // 500 (min)  < min_price < 1000 (max)
+  // $lt  $gt $lte $gte
+  // min_price:{ $lte:1000, $gte:500 }
+  // sort => 1 (low to high) , -1 (high to low)
+  // 2st => 2
+  // [100,200, 300,400, 500,600, 700,800]
+  //    1          2       3       3
+  let data = {
+    page: 2,
+    perPage: 2,
+  };
+  let sort = data.sort ? data.sort : 1;
   let filter = {}; // runtime create
+  let startIndex = data.page * data.perPage - data.perPage; // 2
+  let lastIndex = data.page * data.perPage; //2
 
   if (data.mealtype !== undefined) filter["mealtype_id"] = data.mealtype;
   if (data.location !== undefined) filter["location_id"] = data.location;
+  if (data.cuisine !== undefined) filter["cuisine_id"] = { $in: data.cuisine };
+  if (data.minCost !== undefined && data.maxConst !== undefined)
+    filter["min_price"] = { $lte: data.maxConst, $gte: data.minCost };
+
   // it must not equals to undefined
   console.log(filter);
 
-  let result = await RestaurantModel.find(filter);
+  let result = await RestaurantModel.find(filter).sort({
+    min_price: sort,
+  });
+  result = result.slice(startIndex, lastIndex);
   response.send({ status: true, result });
 };
